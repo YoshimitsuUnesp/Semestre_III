0fe88d1fcc925a9ee1da642362bd7e13f0685d08
YoshimitsuUnesp <yoshimitsu.lanzo@unesp.br>
2022-05-24 20:42:30 -0300

    Comeco do Trabalho 1 - Implementando as classes base que serao utilizadas

---
+++ LPOO/Trabalho1/Passageiro.java
@@ -0,0 +1,28 @@
+public class Passageiro {
+    private String documentoId; // Deixando documentoId como String por considerar ter 11 digitos
+    private String nome;
+    private Data dataNasc;
+    private String endereco; // Se sobrar tempo, implementar classe Endereco
+
+
+    //Construtor
+    public Passageiro(String documentoId, String nome, Data dataNasc, String endereco) {
+        this.documentoId = documentoId;
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.endereco = endereco;
+    }
+
+    // Getters e Setters
+    public String getdocumentoId() { return documentoId; }
+    public void setdocumentoId(String documentoId) { this.documentoId = documentoId; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public Data getDataNasc() { return dataNasc; }
+    public void setDataNasc(Data dataNasc) { this.dataNasc = dataNasc; }
+
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+}
---
+++ LPOO/Trabalho1/Rotas.java
@@ -0,0 +1,32 @@
+public class Rotas {
+    private String origem;
+    private String parada;
+    private String destino;
+    private Hora horaSaida;
+    private Hora horaChegada;
+
+    // Construtor
+    public Rotas(String origem, String parada, String destino, Hora horaSaida, Hora horaChegada) {
+        this.origem = origem;
+        this.parada = parada;
+        this.destino = destino;
+        this.horaSaida = horaSaida;
+        this.horaChegada = horaChegada;
+    }
+
+    // Getters e setters
+    public String getOrigem() { return origem; }
+    public void setOrigem(String origem) { this.origem = origem; }
+
+    public String getParada() { return parada; }
+    public void setParada(String parada) { this.parada = parada; }
+
+    public String getDestino() { return destino; }
+    public void setDestino(String destino) { this.destino = destino; }
+
+    public Hora getHoraSaida() { return horaSaida; }
+    public void setHoraSaida(Hora horaSaida) { this.horaSaida = horaSaida; }
+
+    public Hora getHoraChegada() { return horaChegada; }
+    public void setHoraChegada(Hora horaChegada) { this.horaChegada = horaChegada; }
+}
